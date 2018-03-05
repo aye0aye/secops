a48206fda65854efb2d1d1cb1feda4f18323a00c
Avi Cavale <avi@shippable.com>
2018-03-05 12:45:29 +0530

    moving packer to script yml

--- infra/awsBuildAMI/baseInit.sh
+++ infra/awsBuildAMI/baseInit.sh
@@ -18,22 +18,17 @@ main() {
     ca-certificates \
     curl \
     software-properties-common -y
-#
-#  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#
-#  sudo add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
-#
-#  sudo apt-get update
-#
-#  sudo apt-get --assume-yes install docker-ce
-#
-#  sudo docker pull hello-world
 
-  echo $BE_IMG
-  echo $BE_TAG
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+  sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+  sudo apt-get update -y
+  sudo apt-get install docker-ce -y
+  sudo docker pull $BE_IMG:$BE_TAG
 }
 
 main
